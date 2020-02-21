d2645403705f58f13686888f561f941b334a3533
benjamesfleming <benfleming121@gmail.com>
2020-02-21 05:54:31 +0000

    add custom html.js

---
+++ src/html.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function HTML(props) {
+    return (
+        <html {...props.htmlAttributes}>
+            <head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                {props.headComponents}
+            </head>
+            <body {...props.bodyAttributes}>
+                {props.preBodyComponents}
+                <div
+                    key={`body`} id="___gatsby"
+                    dangerouslySetInnerHTML={{ __html: props.body }}
+                />
+                {props.postBodyComponents}
+            </body>
+            <footer className="max-w-3xl mx-auto">
+                © Ben Fleming 2020-{new Date().getFullYear()}. All Rights Reserved.
+            </footer>
+        </html>
+    )
+};
