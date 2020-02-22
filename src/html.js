558695a2f81a02a419f07213708daf99b1f34449
benjamesfleming <benfleming121@gmail.com>
2020-02-22 00:42:45 +0000

    move footer into body

--- src/html.js
+++ src/html.js
@@ -20,11 +20,11 @@ export default function HTML(props) {
                     dangerouslySetInnerHTML={{ __html: props.body }}
                 />
                 {props.postBodyComponents}
+                <footer className="max-w-3xl mx-auto p-6 pt-0">
+                    © Ben Fleming 2020-{new Date().getFullYear()}. All Rights
+                    Reserved.
+                </footer>
             </body>
-            <footer className="max-w-3xl mx-auto p-6 pt-0">
-                © Ben Fleming 2020-{new Date().getFullYear()}. All Rights
-                Reserved.
-            </footer>
         </html>
     );
 }
