b20352cd5435d8f4721a8e991257d53be7897fc9
benjamesfleming <benfleming121@gmail.com>
2020-02-23 06:49:10 +0000

    move padding to header content

--- src/layout.js
+++ src/layout.js
@@ -3,8 +3,8 @@ import React from "react";
 export default ({ header, content }) => {
     return (
         <div className="layout">
-            <div className="header w-full p-6 bg-black text-white shadow-lg">
-                <div className="max-w-3xl mx-auto">{header}</div>
+            <div className="header w-full bg-black text-white shadow-lg">
+                <div className="max-w-3xl mx-auto p-6">{header}</div>
             </div>
             <div className="content max-w-3xl mx-auto p-6 overflow-hidden">
                 {content}
