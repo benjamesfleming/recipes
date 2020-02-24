6b6e74a13b899354ffcfcc911e714c18aa337113
Ben Fleming <benfleming121@gmail.com>
2020-02-24 20:03:58 +1300

    move recipes into root dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `recipes`,
-                path: `${__dirname}/src/recipes`,
+                path: `${__dirname}/recipes`,
             },
         },
         `gatsby-transformer-remark`,
